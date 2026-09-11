d8a95e2b0d39d216afcd0d314051d7febb19dad5
marigold <phelixgluy@gmail.com>
2026-09-11 23:02:41 +0500

    add: build.zig

--- build.zig
+++ build.zig
@@ -1,3 +1,25 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {}
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "asciit",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+
+            .error_tracing = null,
+            .omit_frame_pointer = true,
+            .stack_check = false,
+            .single_threaded = true,
+            .strip = true,
+            .unwind_tables = .none,
+            .stack_protector = false,
+        }),
+    });
+
+    b.installArtifact(exe);
+}
